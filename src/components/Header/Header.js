adc72ed0c07323b95f9f68eb5c45976c69ff91a1
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-17 22:45:15 -0400

    create header just for vision not sure if perm

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './Header.css'
 
+
+const API_URL = 'https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple'
+
 export class Header extends Component {
     render() {
         return (
