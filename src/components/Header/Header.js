619f89c8259f096271a6cd7d27fa2afe3e756ede
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-17 21:35:10 -0400

    initial push

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h1>PINKY AND THE BRAINS</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
