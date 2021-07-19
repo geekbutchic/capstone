5a8fcffc2eff4586a45cb5e9290a6c7f3ab1a7b7
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-18 22:41:43 -0400

    updates for container question and answers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from './components/Header/Header'
+import Body from './components/Body/Body'
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <Body/>
       </div>
     )
   }
---
+++ src/components/Body/Body.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div className='container'>
+            <div>
+                THIS BOX CONTAINS THE QUESTION
+            </div>
+            <div>ANSWER A</div>
+            <div>ANSWER B</div>
+            <div>ANSWER C</div>
+            <div>ANSWER D</div>
+        </div>
+    )
+}
+
+export default Body
+
