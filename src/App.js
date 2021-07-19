239aa45b9799be774640887322f0c31607a026a8
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-19 11:04:21 -0400

    cleanup the css

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ const API_URL = 'https://opentdb.com/api.php?amount=20&category=11&difficulty=ea
 function Body() {
     return (
         <div className='container'>
-            <div className='bg-white text-purple-800 p-10 rounded-lg shadow-md'>
+            <div className='bg-white text-purple-800 p-10 rounded-xl shadow-md'>
               <h2 className='text-2xl'>
-              THIS BOX CONTAINS THE QUESTION
+              1. THIS BOX CONTAINS THE QUESTION
               </h2>
             </div>
-            <div className='flex flex-wrap mt-4 justify-around'>  
-            <button className='w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER A</button>
-            <button className='w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER B</button>
-            <button className='w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER C</button>
-            <button className='w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER D</button> 
+            <div className='flex flex-wrap mt-4 justify-around grid gap-x-8 gap-y-4 grid-cols-2'>  
+            <button className='p-4 text-purple-800 font-semibold rounded-xl shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER A</button>
+            <button className='p-4 text-purple-800 font-semibold rounded-xl shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER C</button>
+            <button className='p-4 text-purple-800 font-semibold rounded-xl shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER B</button>
+            <button className='p-4 text-purple-800 font-semibold rounded-xl shadow mb-4 bg-green-500 hover:bg-red-200'>ANSWER D</button> 
             </div>
         </div>
     )
