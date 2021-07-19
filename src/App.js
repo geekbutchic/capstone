616c4bff3162d165c1077c222a3f0baced6af53f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-19 00:01:26 -0400

    update css for container and questions containers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react'
-import Header from './components/Header/Header'
-import Body from './components/Body/Body'
+import React from 'react'
 
-export class App extends Component {
-  render() {
+const API_URL = 'https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple'
+
+
+function Body() {
     return (
-      <div>
-        <Header/>
-        <Body/>
-      </div>
+        <div className='container'>
+            <div className='bg-white text-purple-800 p-10 rounded-lg shadow-md'>
+              <h2 className='text-2xl'>
+              THIS BOX CONTAINS THE QUESTION
+              </h2>
+            </div>
+            <div className='flex flex-wrap mt-4 justify-between'>  
+            <button className='bg-white w-1/2 p-4 text-purple-800 font-semibold rounded shadow'>ANSWER A</button>
+            <button className='bg-white w-1/2 p-4 text-purple-800 font-semibold rounded shadow'>ANSWER B</button>
+            <button className='bg-white w-1/2 p-4 text-purple-800 font-semibold rounded shadow'>ANSWER C</button>
+            <button className='bg-white w-1/2 p-4 text-purple-800 font-semibold rounded shadow'>ANSWER D</button> 
+            </div>
+        </div>
     )
-  }
 }
 
-export default App
+export default Body
