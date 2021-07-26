34f79ebceaa5f7a5e41581d725342149f30f9c73
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-26 00:18:59 -0400

    adjust css , background test new API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'// REACT HOOKS - DECONSTRUCTED 
 import { Questionaire } from './components'; 
 
-const API_URL = 'https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple'//API URL 
+// const API_URL1 = 'https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple'//API URL//MOVIES
+const API_URL = 'https://opentdb.com/api.php?amount=20&category=12&difficulty=easy&type=multiple'//MUSIC
 
 function App() {//SETS STATE FOR HOOKS  
   const [ questions, setQuestions ] = useState([]);
@@ -23,7 +24,8 @@ function App() {//SETS STATE FOR HOOKS
         }));
       setQuestions(questions);
     });
-  }, []); 
+  }, []);
+  
 
   const handleAnswer = (answer) => {
     if(!answers) {
--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -7,17 +7,17 @@ const Questionaire = ({ showAnswers, handleAnswer,handleNextQuestion, data: {que
         <div>
             <h1 className='text-6xl p-8 text-red-600 flex justify-center items-center font-serif Georgia text-white'>Red Hot Trivia Peppers!</h1>
         </div>
-        <div className='bg-white text-black p-8 rounded-xl shadow-md font-semibold'>
+        <div className='bg-red-100 text-black p-8 rounded-xl shadow-md font-semibold'>
         <h2 className='text-2xl' dangerouslySetInnerHTML={{ __html: question }}/>
         </div>
         <div className='flex flex-wrap mt-4 justify-around grid gap-x-8 gap-y-4 grid-cols-2'>
         
-        {answers.map((answer, idx) => {//MAP OVER BUTTONS 
+        {answers.map((answer, index) => {//MAP OVER BUTTONS 
         const bgColor = showAnswers ? answer === correct_answer ? 'bg-green-300' : 'bg-red-300' : 'bg-white';
         return (
         <button
-        key={idx}//KEY FOR PROPS
-        className={`${bgColor} p-4 text-black font-semibold rounded-xl shadow mb-4 bg-white hover:bg-blue-300`}
+        key={index}//KEY FOR PROPS
+        className={`${bgColor} p-4 text-black font-semibold rounded-xl bg-red-100 shadow mb-4 bg-white hover:bg-blue-300`}
         onClick={() => handleAnswer(answer)}
         dangerouslySetInnerHTML={{ __html: answer }}/>
         )})}
