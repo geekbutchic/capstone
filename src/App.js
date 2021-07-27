2072cc1ca9768823b297199512ae64f369f666be
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-07-27 10:34:43 -0400

    adds red color to wrong answer

--- src/App.js
+++ src/App.js
@@ -1,62 +1,65 @@
-import React, { useState, useEffect } from 'react'// REACT HOOKS - DECONSTRUCTED 
-import { Questionaire } from './components'; 
+import React, { useState, useEffect } from "react"; // REACT HOOKS - DECONSTRUCTED
+import { Questionaire } from "./components";
 
 // const API_URL1 = 'https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple'//API URL//MOVIES
-const API_URL = 'https://opentdb.com/api.php?amount=20&category=12&difficulty=easy&type=multiple'//MUSIC
+const API_URL =
+  "https://opentdb.com/api.php?amount=20&category=12&difficulty=easy&type=multiple"; //MUSIC
 
-function App() {//SETS STATE FOR HOOKS  
-  const [ questions, setQuestions ] = useState([]);
-  const [ currentIndex, setCurrentIndex ] = useState(0);
+function App() {
+  //SETS STATE FOR HOOKS
+  const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [value, setScore] = useState(0);
-  const [ answers, setAnswers] = useState(false)
+  const [answers, setAnswers] = useState(false);
 
-  useEffect (() => {
-    fetch(API_URL) 
-    .then((res) => res.json())
-    .then((data) => {
-      const questions = data.results.map((question) => 
-      ({
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const questions = data.results.map((question) => ({
           ...question,
           answers: [
             question.correct_answer,
             ...question.incorrect_answers,
-          ].sort(() => Math.random() - 0.5)
+          ].sort(() => Math.random() - 0.5),
         }));
-      setQuestions(questions);
-    });
+        setQuestions(questions);
+      });
   }, []);
-  
 
   const handleAnswer = (answer) => {
-    if(!answers) {
-    if (answer === questions[currentIndex].correct_answer) {
-      setScore(value + 1); 
-    } 
-  }
-    setAnswers(true)
+    if (!answers) {
+      if (answer === questions[currentIndex].correct_answer) {
+        setScore(value + 1);
+      }
+    }
+    setAnswers(true);
   };
 
   const handleNextQuestion = () => {
     setAnswers(false);
-    setCurrentIndex(currentIndex + 1)
-  }
+    setCurrentIndex(currentIndex + 1);
+  };
 
-return questions.length > 0 ? (    
-     <div className='container'>
-       {currentIndex >= questions.length ? (
-        <h1 className='text-2xl text-white font-bold bg-red-400 rounded-xl p-2'>GAME ENDED YOUR FINAL SCORE IS: {value}/20</h1>
-    ) : (//PASSING IN PROPS 
-      <Questionaire
-      data={questions[currentIndex]}
-      showAnswers={answers}
-      handleNextQuestion={handleNextQuestion}
-      handleAnswer={handleAnswer}
-      />
-    )}
+  return questions.length > 0 ? (
+    <div className="container">
+      {currentIndex >= questions.length ? (
+        <h1 className="text-2xl text-white font-bold bg-red-400 rounded-xl p-2">
+          GAME ENDED YOUR FINAL SCORE IS: {value}/20
+        </h1>
+      ) : (
+        //PASSING IN PROPS
+        <Questionaire
+          data={questions[currentIndex]}
+          showAnswers={answers}
+          handleNextQuestion={handleNextQuestion}
+          handleAnswer={handleAnswer}
+        />
+      )}
     </div>
-    ):(
-        <h1 className='text-2xl text-white font-bold'>Loading...</h1>//SETS LOADING SCREEN IF DELAYED - USUALLY 1 TO 2 SECONDS 
-    );
+  ) : (
+    <h1 className="text-2xl text-white font-bold">Loading...</h1> //SETS LOADING SCREEN IF DELAYED - USUALLY 1 TO 2 SECONDS
+  );
 }
 
-export default App
+export default App;
--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,38 +1,55 @@
-import React from 'react'
+import React from "react";
 
-//DECONSTRUCT FOR HOOKS + DOUBLE DECONSTRUCT 
-const Questionaire = ({ showAnswers, handleAnswer,handleNextQuestion, data: {question, correct_answer, answers} }) => {    
-    return (
+//DECONSTRUCT FOR HOOKS + DOUBLE DECONSTRUCT
+const Questionaire = ({
+  showAnswers,
+  handleAnswer,
+  handleNextQuestion,
+  data: { question, correct_answer, answers },
+}) => {
+  return (
     <div>
-        <div>
-            <h1 className='text-6xl p-8 text-red-600 flex justify-center items-center font-serif Georgia text-white'>Red Hot Trivia Peppers!</h1>
-        </div>
-        <div className='bg-yellow-100 text-black p-8 rounded-xl shadow-md font-semibold'>
-        <h2 className='text-2xl' dangerouslySetInnerHTML={{ __html: question }}/>
-        </div>
-        <div className='flex flex-wrap mt-4 justify-around grid gap-x-8 gap-y-4 grid-cols-2'>
-        
-        {answers.map((answer, index) => {//MAP OVER BUTTONS 
-        const bgColor = showAnswers ? answer === correct_answer ? 'bg-green-300' : 'bg-red-300' : '';
-        return (
-        <button
-        key={index}//KEY FOR PROPS
-        className={`${bgColor} p-4 text-black font-semibold rounded-xl bg-yellow-100 shadow mb-4 bg-white hover:bg-blue-300`}
-        onClick={() => handleAnswer(answer)}
-        dangerouslySetInnerHTML={{ __html: answer }}/>
-        )})}
-
-        </div>
-        {showAnswers && (
-            <div className="flex justify-center items-center">
+      <div>
+        <h1 className="text-6xl p-8 text-red-600 flex justify-center items-center font-serif Georgia text-white">
+          Red Hot Trivia Peppers!
+        </h1>
+      </div>
+      <div className="bg-green-100 text-black p-8 rounded-xl shadow-md font-semibold">
+        <h2
+          className="text-2xl"
+          dangerouslySetInnerHTML={{ __html: question }}
+        />
+      </div>
+      <div className="flex flex-wrap mt-4 justify-around grid gap-x-8 gap-y-4 grid-cols-2">
+        {answers.map((answer, index) => {
+          //MAP OVER BUTTONS
+          const bgColor = showAnswers
+            ? answer === correct_answer
+              ? "bg-green-300"
+              : "bg-red-300"
+            : "bg-green-100";
+          return (
             <button
+              key={index} //KEY FOR PROPS
+              className={`${bgColor} p-4 text-black font-semibold rounded-xl shadow mb-4 bg-white hover:bg-blue-300`}
+              onClick={() => handleAnswer(answer)}
+              dangerouslySetInnerHTML={{ __html: answer }}
+            />
+          );
+        })}
+      </div>
+      {showAnswers && (
+        <div className="flex justify-center items-center">
+          <button
             onClick={handleNextQuestion}
-            className={`p-3 text-black font-semibold rounded-xl shadow mb-4 bg-blue-200 hover:bg-blue-400`}>
+            className={`p-3 text-black font-semibold rounded-xl shadow mb-4 bg-blue-200 hover:bg-blue-400`}
+          >
             Next Question
-            </button>
-            </div>
-        )}
+          </button>
+        </div>
+      )}
     </div>
-)};     
+  );
+};
 
 export default Questionaire;
